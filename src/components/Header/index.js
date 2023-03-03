7ac9312912777c6e7d1065d06720b41548aa9730
TrungPD <dgranin8012000@gmail.com>
2023-03-03 12:00:10 +0700

    Complete Footer

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4"></div>
+                </div>
+            </div>
+        </header>
+    );
+}
